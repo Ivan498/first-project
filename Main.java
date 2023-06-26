0f50c240760e3fafa7c0821d676ce5caa440048c
Ivan <vankar2824@mail.ru>
2023-06-26 16:15:09 +0300

    ProMain

--- Main.java
+++ Main.java
@@ -30,10 +30,11 @@ public class Main {
 
     }
     static void printMenu(){
-        System.out.println("1 - Ввести количество шагов за определённый день");
-        System.out.println("2 - Изменить цель по количеству шагов в день");
-        System.out.println("3 - Напечатать статистику за определённый месяц");
-        System.out.println("4 - Выйти из приложения");
+        System.out.println("1 - Ввести количество шагов за определённый день.");
+        System.out.println("2 - Изменить цель по количеству шагов в день.");
+        System.out.println("3 - Напечатать статистику за определённый месяц.");
+        System.out.println("4 - Выйти из приложения." +
+                "");
     }
 
 }
